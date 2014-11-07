4d7ebb6fd6e050e0e233d388892e349dd24c0457
Fahad Almusharraf <ifahad7@gmail.com>
2014-11-06 21:38:13 -0800

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Fur::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
+  # devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
